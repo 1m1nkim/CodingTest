72c3e94625f2fdff75eefac0ef92cc33da2a630c
1m1nkim <130281920+1m1nkim@users.noreply.github.com>
2024-12-09 17:34:06 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.46 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,27 +1,27 @@
 class Solution {
     public int[] solution(int[] arr) {
-        // 배열 길이가 1일 경우 [-1] 반환
-        if (arr.length == 1) {
+        int[] answer = new int[arr.length-1];
+        int min = arr[0];
+        int j=0;
+        if(arr.length == 1){
             return new int[]{-1};
         }
-
-        // 최소값 찾기
-        int min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (min > arr[i]) {
+        for(int i=1; i<arr.length; i++){
+            if(min > arr[i]){
                 min = arr[i];
             }
         }
-
-        // 최소값을 제외한 배열 생성
-        int[] answer = new int[arr.length - 1];
-        int index = 0; // answer 배열의 인덱스
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != min) {
-                answer[index++] = arr[i];
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] != min){
+                answer[j++] = arr[i];
             }
         }
-
         return answer;
     }
 }
+
+/*
+      int min = Arrays.stream(arr).min().getAsInt();
+      return Arrays.stream(arr).filter(i -> i != min).toArray();
+      이게 더 좋은 코드 같음
+*/
